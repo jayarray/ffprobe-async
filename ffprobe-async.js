c4f7ca2c2c5715d6661ace30c870a1275991523d
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-01-28 11:58:34 -0800

    Refactored code to reflect path check.

--- ffprobe-async.js
+++ ffprobe-async.js
@@ -1,5 +1,4 @@
-let PATH = require('path');
-let CHILD_PROCESS = require('child_process');
+let FILESYSTEM = ('filesystem-async');
 
 //-----------------------------------
 // ERROR CATCHING
@@ -9,163 +8,213 @@ function fatalFail(error) {
   process.exit(-1);
 }
 
-//-----------------------------------
-// SAVING DATA (to string)
-
-class SavedData {
-  constructor(thing) {
-    this.value = '';
-    thing.on('data', this.callback_.bind(this));
-  }
-
-  callback_(data) {
-    this.value += data.toString();
-  }
-}
-
-//-----------------------------------
-// EXECUTE
-
-function execute(cmd, args) {
-  let childProcess = CHILD_PROCESS.spawn(cmd, args);
-  let errors = new SavedData(childProcess.stderr);
-  let outputs = new SavedData(childProcess.stdout);
-
-  return new Promise(resolve => {
-    childProcess.on('close', exitCode => {
-      resolve({
-        stderr: errors.value,
-        stdout: outputs.value,
-        exitCode: exitCode
-      });
-    });
-  });
-}
-
 //------------------------------------
 // FUNCTIONS
 
-function codec_types(src) { // returns audio, video, or both
+function codec_types(src) { // returns 'audio', 'video', or both
   return new Promise(resolve => {
-    let args = `-v error -show_entries stream=codec_type -of default=nw=1 ${src}`.split(' '); // If fails, put double-quotes around src
-    execute('ffprobe', args).then(results => {
-      if (results.stderr) {
-        resolve({ types: null, error: results.stderr });
+    FILESYSTEM.Path.exists().then(results => {
+      if (results.error) {
+        reject({ types: null, error: results.error });
         return;
       }
 
-      let types = [];
+      if (!results.exists) {
+        reject({ types: null, error: `Path does not exist: ${src}` });
+        return;
+      }
 
-      let lines = results.stdout;
-      lines.forEach(line => {
-        if (line.includes('audio'))
-          types.push('audio');
-        else if (line.includes('video'))
-          types.push('video');
-      });
-      resolve({ types: types, error: null });
+      let args = ['-v', 'error', '-show_entries', 'stream=codec_type', '-of', 'default=nw=1', src.trim()]; // If fails, put double-quotes around src
+      FILESYSTEM.Execute.local('ffprobe', args).then(results => {
+        if (results.stderr) {
+          resolve({ types: null, error: results.stderr });
+          return;
+        }
+
+        let types = [];
+
+        let lines = results.stdout;
+        lines.forEach(line => {
+          if (line.includes('audio'))
+            types.push('audio');
+          else if (line.includes('video'))
+            types.push('video');
+        });
+        resolve({ types: types, error: null });
+      }).catch(fatalFail);
     }).catch(fatalFail);
   });
 }
 
 function is_video(src) {
   return new Promise(resolve => {
-    codec_types(src).then(results => {
+    FILESYSTEM.Path.exists(src).then(results => {
       if (results.error) {
-        resolve({ isVideo: null, error: results.error });
+        reject({ types: null, error: results.error });
         return;
       }
-      resolve({ isVideo: results.types.includes('video'), error: results.error });
+
+      if (!results.exists) {
+        reject({ types: null, error: `Path does not exist: ${src}` });
+        return;
+      }
+
+      codec_types(src.trim()).then(results => {
+        if (results.error) {
+          resolve({ isVideo: null, error: results.error });
+          return;
+        }
+        resolve({ isVideo: results.types.includes('video'), error: results.error });
+      }).catch(fatalFail);
     }).catch(fatalFail);
   });
 }
 
 function is_audio(src) {
   return new Promise(resolve => {
-    codec_types(src).then(results => {
+    FILESYSTEM.Path.exists(src).then(results => {
       if (results.error) {
-        resolve({ isVideo: null, error: results.error });
+        reject({ types: null, error: results.error });
         return;
       }
-      resolve({
-        isAudio: results.types.includes('audio') && !results.types.includes('video'),
-        error: results.error
-      });
-    }).catch(fatalFail);
+
+      if (!results.exists) {
+        reject({ types: null, error: `Path does not exist: ${src}` });
+        return;
+      }
+
+      codec_types(src.trim()).then(results => {
+        if (results.error) {
+          resolve({ isVideo: null, error: results.error });
+          return;
+        }
+        resolve({
+          isAudio: results.types.includes('audio') && !results.types.includes('video'),
+          error: results.error
+        });
+      }).catch(fatalFail);
+    });
   });
 }
 
 function duration_string(src) {
   return new Promise(resolve => {
-    let args = `-i ${src} -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 -sexagesimal`.split(' ');
-    execute('ffprobe', args).then(results => {
-      if (results.stderr) {
-        resolve({ seconds: null, error: results.stderr });
+    FILESYSTEM.Path.exists(src).then(results => {
+      if (results.error) {
+        reject({ types: null, error: results.error });
         return;
       }
-      resolve({ string: results.stdout.trim(), error: null });
-    }).catch(fatalFail);
+
+      if (!results.exists) {
+        reject({ types: null, error: `Path does not exist: ${src}` });
+        return;
+      }
+
+      let args = ['-i', src.trim(), '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', '-sexagesimal'];
+      FILESYSTEM.Execute.local('ffprobe', args).then(results => {
+        if (results.stderr) {
+          resolve({ seconds: null, error: results.stderr });
+          return;
+        }
+        resolve({ string: results.stdout.trim(), error: null });
+      }).catch(fatalFail);
+    });
   });
 }
 
 function duration_time_units(src) {
   return new Promise(resolve => {
-    duration_string(src).then(results => {
+    FILESYSTEM.Path.exists(src).then(results => {
       if (results.error) {
-        resolve({ units: null, error: results.error });
+        reject({ types: null, error: results.error });
         return;
       }
 
-      if (results.string.trim() && results.string.split(':').length == 3) {
-        let parts = results.stdout.trim().split(':');
-        let hours = parseFloat(parts[0]);
-        let minutes = parseFloat(parts[1]);
-        let seconds = parseFloat(parts[2].slice(0, 5).join(''));
-        resolve({
-          units: { hours: hours, minutes: minutes, seconds: seconds },  // float, float, float
-          error: null
-        });
+      if (!results.exists) {
+        reject({ types: null, error: `Path does not exist: ${src}` });
         return;
       }
-      resolve({ units: null, error: null }); // No string returned
-    }).catch(fatalFail);
+
+      duration_string(src.trim()).then(results => {
+        if (results.error) {
+          resolve({ units: null, error: results.error });
+          return;
+        }
+
+        if (results.string.trim() && results.string.split(':').length == 3) {
+          let parts = results.stdout.trim().split(':');
+          let hours = parseFloat(parts[0]);
+          let minutes = parseFloat(parts[1]);
+          let seconds = parseFloat(parts[2].slice(0, 5).join(''));
+          resolve({
+            units: { hours: hours, minutes: minutes, seconds: seconds },  // float, float, float
+            error: null
+          });
+          return;
+        }
+        resolve({ units: null, error: null }); // No string returned
+      }).catch(fatalFail);
+    });
   });
 }
 
 function duration_in_seconds(src) {  // in seconds
   return new Promise(resolve => {
-    duration_string(src).then(results => {
+    FILESYSTEM.Path.exists(src).then(results => {
       if (results.error) {
-        resolve({ seconds: null, error: results.error });
+        reject({ types: null, error: results.error });
         return;
       }
 
-      if (results.string.trim() && results.string.split(':').length == 3) {
-        let parts = results.stdout.trim().split(':');
-        let hours = parseFloat(parts[0]);
-        let minutes = parseFloat(parts[1]);
-        let seconds = parseFloat(parts[2].slice(0, 5).join(''));
-        resolve({ seconds: (hours * 3600) + (minutes * 60) + (seconds), error: null });  // float
+      if (!results.exists) {
+        reject({ types: null, error: `Path does not exist: ${src}` });
         return;
       }
-      resolve({ seconds: null, error: null }); // No string returned
-    }).catch(fatalFail);
+
+      duration_string(src.trim()).then(results => {
+        if (results.error) {
+          resolve({ seconds: null, error: results.error });
+          return;
+        }
+
+        if (results.string.trim() && results.string.split(':').length == 3) {
+          let parts = results.stdout.trim().split(':');
+          let hours = parseFloat(parts[0]);
+          let minutes = parseFloat(parts[1]);
+          let seconds = parseFloat(parts[2].slice(0, 5).join(''));
+          resolve({ seconds: (hours * 3600) + (minutes * 60) + seconds, error: null });  // float
+          return;
+        }
+        resolve({ seconds: null, error: null }); // No string returned
+      }).catch(fatalFail);
+    });
   });
 }
 
 function info(src) {
   return new Promise(resolve => {
-    let args = `-v quiet -print_format json -show_format -show_streams ${src}`.split(' '); // If fails, put double-quotes around src
-    execute('ffprobe', args).then(results => {
-      if (results.stderr) {
-        resolve({ info: null, error: results.stderr });
+    FILESYSTEM.Path.exists(src).then(results => {
+      if (results.error) {
+        reject({ types: null, error: results.error });
         return;
       }
-      resolve({ info: JSON.parse(results.stdout), error: null }); // returns { "streams": {...}, "formats": {...} }
-    }).catch(fatalFail);
+
+      if (!results.exists) {
+        reject({ types: null, error: `Path does not exist: ${src}` });
+        return;
+      }
+
+      let args = ['-v', 'quiet', '-print_format', 'json', '-show_format', '-show_streams', src.trim()];
+      FILESYSTEM.Execute.local('ffprobe', args).then(results => {
+        if (results.stderr) {
+          resolve({ info: null, error: results.stderr });
+          return;
+        }
+        resolve({ info: JSON.parse(results.stdout), error: null }); // returns { "streams": {...}, "formats": {...} }
+      }).catch(fatalFail);
+    });
   });
 }
-
 
 //------------------------------------
 // EXPORTS
